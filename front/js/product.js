1aff064a13b5e25b73da1e8a7beb9fbc73b431fa
Nyarrha <paradisquentin@yahoo.fr>
2022-01-21 00:10:40 +0100

    Fin page produit JS

--- front/js/product.js
+++ front/js/product.js
@@ -2,6 +2,10 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id')
+if (id != null) {
+    let kanapPrice = 0
+    let imgUrl, altText, productName
+}
 
 // Appel API du produit sélectionné
 fetch(`http://localhost:3000/api/products/${id}`)
@@ -14,6 +18,10 @@ fetch(`http://localhost:3000/api/products/${id}`)
 // Fonction regroupant les différentes données du produits et les fonctions pour les afficher
 function recupData(kanap){
     const {altTxt, colors, description, imageUrl, name, price} = kanap;
+    kanapPrice = price
+    imgUrl = imageUrl
+    altText = altTxt
+    productName = name
     createImage(imageUrl, altTxt)
     createTitle(name)
     createPrice(price)
@@ -62,4 +70,32 @@ function createColors(colors){
         select.appendChild(option)
         })
     }
+}
+
+// Création variable pour créer l'EventListener
+const button = document.querySelector('#addToCart')
+if (button != null) {
+    // Création EventListener du clic sur le bouton et variables pour stocker les choix utilisateur
+button.addEventListener('click', (e) => {
+    const colorChoice = document.querySelector('#colors').value
+    const quantityChoice = document.querySelector('#quantity').value
+    console.log(colorChoice, quantityChoice)
+    // Traitement des différents cas d'erreur(s'il n'y a pas de couleur ou de quantité choisie, par exemple)
+    if (colorChoice == null || colorChoice === '' || quantityChoice == null || quantityChoice == 0) {
+    alert('Veuillez choisir une couleur et une quantité')
+    return
+    }
+
+    // Création objet à stocker dans le localStorage
+    const data = {
+        id : id,
+        color : colorChoice,
+        quantity : Number(quantityChoice),
+        imageUrl : imgUrl,
+        altTxt : altText
+    }
+    // Conversion de l'objet en JSON, stockage dans le localStorage puis redirection vers la page panier
+    localStorage.setItem(id, JSON.stringify(data))
+    window.location.href = 'cart.html'  
+})
 }
