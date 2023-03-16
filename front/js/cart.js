2554bdc3b1011f4f065a05a43d8dda644fc9e9d7
Nyarrha <paradisquentin@yahoo.fr>
2023-03-16 17:57:24 +0100

    Avancée affichage page panier + ajout commentaires

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,11 +2,14 @@ let url = `http://localhost:3000/api/products/`
 let cart = JSON.parse(window.localStorage.getItem('cart')) ?? [];
 let res = await fetch(url);
 let products = await res.json();
-cart.forEach(item => console.log(item));
 
 function createCartItem() {
     let section = document.querySelector('#cart__items')
     // Appel fonctions pour créer l'item panier
+    let article = createArticle();
+    let image = createImage();
+    let itemsContent = createItemsContent();
+    // -------- Pas fini --------- //
 }
 
 // Création de la balise article
@@ -22,15 +25,17 @@ function createArticle() {
 function createImage() {
     let divImage = document.createElement('div')
     divImage.classList.add('cart__item__img');
+
     let img = document.createElement('img');
     img.src = cart.imageUrl;
     img.alt = cart.altTxt;
     divImage.appendChild(img);
+
     return divImage;
 }
 
 // Création de la grande div contenant les autres concernant produit
-function createContent() {
+function createItemsContent() {
     let divContent = document.createElement('div');
     divContent.classList.add('cart__item__content');
     // Regroupement des div dans la plus grande
@@ -45,8 +50,10 @@ function createContent() {
     return divContent;
 }
 
-function createContent() {
+// Création de la div description du produit
+function createItemContent() {
     let divDesc = document.createElement('div');
+    divDesc.classList.add('cart__item__content__description');
     let title = document.createElement('h2');
     title.textContent = products.name
 
@@ -59,9 +66,65 @@ function createContent() {
     divDesc.appendChild(title);
     divDesc.appendChild(color);
     divDesc.appendChild(price);
+
     return divDesc;
 }
 
+
+// Création div regroupant éléments concernant les paramètres de la commande
 function createContentSettings() {
-    // A suivre
+    let settings = document.createElement('div');
+    settings.classList.add('cart__item__content__settings');
+
+    let settingsQuantity = createSettingsQuantity();
+    let settingsDelete = settingsDelete();
+
+    settings.appendChild(settingsQuantity);
+    settings.appendChild(settingsDelete);
+
+    return settings;
+}
+
+// Fonction créant les éléments de paramètres directement rattachés à la div au dessus
+function createSettingsQuantity() {
+    let settingsQuantity = document.createElement('div');
+    settingsQuantity.classList.add('cart__item__content__settings__quantity');
+
+    let quantityText = document.createElement('p');
+    quantityText.textContent = 'Quantité : ';
+
+    let quantityButton = document.createElement('input');
+    quantityButton.type = 'number';
+    quantityButton.classList.add('itemQuantity');
+    quantityButton.name = 'itemQuantity';
+    quantityButton.min = '1';
+    quantityButton.max = '100';
+    quantityButton.value = parseInt(cart.quantityChoice);
+
+    settingsQuantity.appendChild(quantityText);
+    settingsQuantity.appendChild(quantityButton);
+
+    return settingsQuantity;
+}
+
+
+// Fonction pour ajouter la proposition de supprimer le produit du produit(seulement l'élément texte)
+function settingsDelete() {
+    let settingsDelete = document.createElement('div');
+    settingsDelete.classList.add('cart__item__content__settings__delete');
+    let settingsDeleteText = document.createElement('p');
+    settingsDeleteText.textContent = 'Supprimer';
+
+    settingsDelete.appendChild(settingsDeleteText);
+
+    return settingsDelete;
+}
+
+
+// Fonction pour calculer la quantité totale d'articles du panier
+function totalQuantity(){
+    let totalQuantity = document.querySelector('#totalQuantity'); 
+    const mapQuantities = cart.map(quantities => quantities.quantity);
+    console.log(mapPrices);
+    totalQuantity.textContent += parseInt(mapQuantities.value);
 }
